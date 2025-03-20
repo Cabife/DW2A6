776c9e8853497387d1f3a19a04d654603eb2db6c
cj3017281 <cj3017281@192.168.56.1>
2025-03-20 20:03:20 -0300

    Aula 20-03-2025

---
+++ IFitness/src/main/java/br/edu/ifspcjo/ads/dw2IFitness/model/Gender.java
@@ -0,0 +1,27 @@
+package br.edu.ifspcjo.ads.dw2IFitness.model;
+
+public enum Gender {
+	
+	MASCULINO("Masculino"),
+	FEMININO("Feminino"),
+	OUTRO("Outro"),
+	PREFIRO_NAO_DIZER("Prefiro não dizer");
+	
+	private String description;
+	
+	private Gender(String description) {
+		this.setDescription(description);
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
+
+
+
+
---
+++ IFitness/src/main/java/br/edu/ifspcjo/ads/dw2IFitness/resources/UserResources.java
@@ -0,0 +1,28 @@
+package br.edu.ifspcjo.ads.dw2IFitness.resources;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+
+
+
+
+
+@RestController
+public class UserResources {
+
+	@GetMapping("\users")
+	public list<user>list()
+	{
+		User user1 = new User();
+	}
+	
+	
+	
+	
+	
+	
+}
